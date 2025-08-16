c3a4e62b077c50f6011ebbbffdebdc8a8e3dcf57
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-08-16 21:01:39 +0600

    Implement Workout API: Add WorkoutAPIView for retrieving workouts based on user gender and category; include WorkoutSerializer and update URL routing.

--- apps/workouts/views.py
+++ apps/workouts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from apps.workouts.models import Workout
+from rest_framework.views import APIView
+from apps.workouts.serializers import WorkoutSerializer
+from apps.utils.helpers import success, error
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class WorkoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        user = request.user
+        gender = user.gender
+
+        category_id = request.query_params.get("category_id", None)
+        print(category_id)
+
+        workouts = Workout.objects.filter(category=category_id, gender=gender) if category_id else Workout.objects.all()
+
+        serializer = WorkoutSerializer(workouts, many=True)
+        print(serializer.data)
+        return success(serializer.data, "Workouts retrieved successfully.", 200)
